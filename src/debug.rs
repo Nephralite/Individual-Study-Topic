c6a66fc8814faadea4350e40f8b243b27b5da46a
Jade Ellingworth <88789529+Nephralite@users.noreply.github.com>
2023-04-24 04:27:07 +0100

    Create debug.rs

---
+++ src/debug.rs
@@ -0,0 +1,49 @@
+use ash::vk;
+
+pub(crate) struct Debug {
+    utils: ash::extensions::ext::DebugUtils,
+    messenger: vk::DebugUtilsMessengerEXT,
+}
+
+impl Debug {
+    pub(crate) fn init(entry: &ash::Entry, instance: &ash::Instance) -> Result<Debug, vk::Result> {
+        let utils = ash::extensions::ext::DebugUtils::new(entry, instance);
+        let debug_create_info = vk::DebugUtilsMessengerCreateInfoEXT::builder()
+            .message_severity(
+                vk::DebugUtilsMessageSeverityFlagsEXT::WARNING
+                    | vk::DebugUtilsMessageSeverityFlagsEXT::VERBOSE
+                    | vk::DebugUtilsMessageSeverityFlagsEXT::INFO
+                    | vk::DebugUtilsMessageSeverityFlagsEXT::ERROR,
+            )
+            .message_type(
+                vk::DebugUtilsMessageTypeFlagsEXT::GENERAL
+                    | vk::DebugUtilsMessageTypeFlagsEXT::PERFORMANCE
+                    | vk::DebugUtilsMessageTypeFlagsEXT::VALIDATION,
+            )
+            .pfn_user_callback(Some(vulkan_debug_utils_callback));
+        let messenger = unsafe { utils.create_debug_utils_messenger(&debug_create_info, None)? };
+        Ok(Debug { utils, messenger })
+    }
+}
+
+impl Drop for Debug {
+    fn drop(&mut self) {
+        unsafe {
+            self.utils
+                .destroy_debug_utils_messenger(self.messenger, None)
+        };
+    }
+}
+
+pub unsafe extern "system" fn vulkan_debug_utils_callback(
+    message_severity: vk::DebugUtilsMessageSeverityFlagsEXT,
+    message_type: vk::DebugUtilsMessageTypeFlagsEXT,
+    p_callback_data: *const vk::DebugUtilsMessengerCallbackDataEXT,
+    _p_user_data: *mut std::ffi::c_void,
+) -> vk::Bool32 {
+    let message = std::ffi::CStr::from_ptr((*p_callback_data).p_message);
+    let severity = format!("{:?}", message_severity).to_lowercase();
+    let ty = format!("{:?}", message_type).to_lowercase();
+    println!("[Debug][{}][{}] {:?}", severity, ty, message);
+    vk::FALSE
+}
